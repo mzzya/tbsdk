8f8238e65672a818373cc4c88b3c0c178b8bd7ac
smgqk <657176605@qq.com>
2018-12-19 00:27:06 +0800

    签名测试

--- tbclient.go
+++ tbclient.go
@@ -1,37 +1,120 @@
 package tbsdk
 
 import (
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"encoding/xml"
+	"io/ioutil"
+	"net/http"
+	"strings"
 	"time"
 )
 
 type Client struct {
-	userHttps bool
-	appKey    string
-	appSecret string
-	APIAddr   string
-	Timeout   time.Duration
+	appKey     string
+	appSecret  string
+	APIAddr    string
+	Timeout    time.Duration
+	Formart    string
+	SignMethod string
+}
+
+type BaseRequest interface {
+	GetAPIName() string
+	GetParams() map[string]interface{}
 }
 
 const (
-	APIAddr     = "gw.api.taobao.com/router/rest"
-	APIAddrTest = "gw.api.tbsandbox.com/router/rest"
+	APIAddr         = "http://gw.api.taobao.com/router/rest"
+	APIAddrTest     = "http://gw.api.tbsandbox.com/router/rest"
+	Formart_Json    = "json"
+	Formart_XML     = "xml"
+	SignMethod_HMAC = "hmac"
+	SignMethod_MD5  = "md5"
 )
 
-func NewClientWithProxy(useHttps bool, appkey, appsecret, apiAddr string) *Client {
+func NewClientWithProxy(appkey, appsecret, apiAddr string) *Client {
 	client := new(Client)
-	client.userHttps = useHttps
 	client.appKey = appkey
 	client.appSecret = appsecret
 	client.APIAddr = apiAddr
+	client.Timeout = 30 * time.Second
+	client.Formart = Formart_Json
+	client.SignMethod = SignMethod_MD5
 	return client
 }
 
-func NewClient(useHttps bool, appkey, appsecret string) *Client {
-	return NewClientWithProxy(useHttps, appkey, appsecret, APIAddr)
+func NewClient(appkey, appsecret string) *Client {
+	return NewClientWithProxy(appkey, appsecret, APIAddr)
 }
 
-func (cli *Client) DoPost(v interface{}) map[string]interface{} {
-	// httpClient := &http.Client{}
-	// httpClient
+func (cli *Client) DoPost(req BaseRequest, session string) ([]byte, error) {
+	tr := &http.Transport{
+		DisableCompression: true,
+	}
+	httpCli := http.Client{Transport: tr, Timeout: cli.Timeout}
+
+	httpReq, err := http.NewRequest("POST", cli.APIAddr, strings.NewReader("name=cjb"))
+	if err != nil {
+		return nil, err
+	}
+	var param = make(map[string]string, 10)
+	param["method"] = req.GetAPIName()
+	param["app_key"] = cli.appKey
+	param["session"] = session
+	param["timestamp"] = time.Now().Format("2006-01-02 15:04:05")
+	param["format"] = cli.Formart
+	param["v"] = "2.0"
+	// partner_id	String	否	合作伙伴身份标识。
+	// target_app_key	String	否	被调用的目标AppKey，仅当被调用的API为第三方ISV提供时有效。
+	if cli.Formart == Formart_Json {
+		param["simplify"] = "true"
+	}
+
+	param["sign_method"] = cli.SignMethod
+	param["sign"] = ""
+	httpReq.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	httpReq.Header.Set("Cookie", "name=anny")
+
+	resp, err := httpCli.Do(httpReq)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
+}
+
+func SignString(params string, appSecret string, signMethod string) string {
+	h := md5.New()
+	h.Write([]byte(appSecret))
+	h.Write([]byte(params)) // 需要加密的字符串为 123456
+	h.Write([]byte(appSecret))
+	cipherStr := h.Sum(nil)
+	return hex.EncodeToString(cipherStr)
+}
+
+func GetResonseString(bytes []byte, err error) (string, error) {
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), err
+}
+
+func GetResonseObject(bytes []byte, err error, v interface{}) error {
+	if err != nil {
+		return err
+	}
+	firstStrIndex := 0
+	for _, bt := range bytes {
+		if bt != ' ' && firstStrIndex == 0 {
+			if bt == '{' {
+				err = json.Unmarshal(bytes, v)
+			} else {
+				err = xml.Unmarshal(bytes, v)
+			}
+			return err
+		}
+	}
 	return nil
 }
--- tbclient_test.go
+++ tbclient_test.go
@@ -1,7 +1,16 @@
 package tbsdk_test
 
-import "testing"
+import (
+	"testing"
 
-func NewClientDoPst(t *testing.T) {
+	"github.com/smgqk/tbsdk"
+)
 
+func TestSignString(t *testing.T) {
+	var param = "app_key12345678fieldsnum_iid,title,nick,price,numformatjsonmethodtaobao.item.seller.getnum_iid11223344sessiontestsign_methodmd5timestamp2016-01-01 12:00:00v2.0"
+	var signResult = "66987CB115214E59E6EC978214934FB8"
+	var signStr = tbsdk.SignString(param, "helloworld", "md5")
+	if signStr != signResult {
+		t.FailNow()
+	}
 }
